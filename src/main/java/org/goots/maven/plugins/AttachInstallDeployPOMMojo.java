b38d965555801ce0f046b7fb0ec2bf3aba9fef10
Nick Cross <ncross@redhat.com>
2017-04-01 22:58:04 +0100

    Refactor to org.goots.maven.plugins

--- src/main/java/org/goots/maven/plugins/AttachInstallDeployPOMMojo.java
+++ src/main/java/org/goots/maven/plugins/AttachInstallDeployPOMMojo.java
@@ -1,11 +1,11 @@
 /*
- * Copyright © 2017 Red Hat, Inc (ncross@redhat.com)
+ * Copyright (C) 2017 Red Hat, Inc (ncross@redhat.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.goots;
+package org.goots.maven.plugins;
 
 import org.apache.maven.artifact.ArtifactUtils;
 import org.apache.maven.artifact.deployer.ArtifactDeployer;
